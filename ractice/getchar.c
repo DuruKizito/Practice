e38ecc470156f8a56fe90de4f653869d93b826d7
Chukwuebuka Kizito Duru <durukizitoc@gmail.com>
2022-10-26 14:17:10 +0000

    the array.c is not running yet, i will figure it out

---
+++ ractice/getchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Verify that the expression getchar ( ) != EOF is 0 or 1 */
+
+int main()
+
+{
+	int c;
+
+	while ((c = getchar()) != EOF)
+		putchar(c);
+}
---
+++ ractice/value_EOF.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* A program to print the value of EOF */
+
+int main()
+{
+
+	putchar(EOF);
+}
